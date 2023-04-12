d0037c8ea22b54a9263bb171dca0bc1b0660eaa6
epaniagua2000 <epaniagua@hawk.iit.edu>
2023-04-11 22:40:23 -0500

    basic map added

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,18 @@ $(document).ready(function(){
         pause: 3000
 			});
 });
+
+/* map */
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
