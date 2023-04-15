2334d188213dc2b9e3ea0a821531a7e12f1a9c86
epaniagua2000 <epaniagua@hawk.iit.edu>
2023-04-15 18:46:01 -0500

    Palace, disney markers added

--- js/javascript.js
+++ js/javascript.js
@@ -12,27 +12,65 @@ $(document).ready(function(){
  
 function initMap() {
   const ghibli = { lat: 35.6962, lng: 139.5704 };
+  const palace = { lat: 35.68587214339364, lng: 139.75387238357166 };
+  const disney = { lat: 35.63309696063062, lng: 139.8808019957778 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
-    center: ghibli,
+    center: palace,
   });
   
-  
-  var marker = new google.maps.Marker({
+  /* Ghibli Marker */
+  var ghiblimarker = new google.maps.Marker({
     position: ghibli,
     map,
     animation: google.maps.Animation.DROP,
     icon: 'media/markerpoint.png',
   });
   
-  var contentString = '<h3>Studio Ghibli Museum</h3>';
+  var ghibliString = "<h2>Studio Ghibli Museum</h2>";
   
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
+  var ghiblititle = new google.maps.InfoWindow({
+    content: ghibliString
   })
   
-  google.maps.event.addListener(marker, 'mouseover', function() {
-    infowindow.open(map, marker);
+  google.maps.event.addListener(ghiblimarker, 'mouseover', function() {
+    ghiblititle.open(map, ghiblimarker);
+  });
+  
+  /* Palace Marker */
+  var palacemarker = new google.maps.Marker({
+    position: palace,
+    map,
+    animation: google.maps.Animation.DROP,
+    icon: 'media/markerpoint.png',
+  });
+  
+  var palaceString = "<h2>Imperial Palace</h2>";
+  
+  var palacetitle = new google.maps.InfoWindow({
+    content: palaceString
+  })
+  
+  google.maps.event.addListener(palacemarker, 'mouseover', function() {
+    palacetitle.open(map, palacemarker);
+  });
+  
+  /* Disney Marker */
+  var disneymarker = new google.maps.Marker({
+    position: disney,
+    map,
+    animation: google.maps.Animation.DROP,
+    icon: 'media/markerpoint.png',
+  });
+  
+  var disneyString = "<h2>Tokyo Disneyland</h2>";
+  
+  var disneytitle = new google.maps.InfoWindow({
+    content: disneyString
+  })
+  
+  google.maps.event.addListener(disneymarker, 'mouseover', function() {
+    disneytitle.open(map, disneymarker);
   });
 }
 
